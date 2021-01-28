94286ad6e835cac8e1b41ade2bcb6a30c5a72e81
bcl31 <17791229+bcl31@users.noreply.github.com>
2021-01-28 02:19:14 -0700

    just a little progress today to get into the swing of this lab.

--- lab2.py
+++ lab2.py
@@ -1,1 +1,14 @@
-# hi
+# this program set was written by James Laidlaw, lab H21.
+# this is a simple Caesar cipher program
+# TODO add more description
+
+def get_input_file():
+    raw_name = input("Please enter the full name of the text file: \n")
+    if raw_name[-4:] == ".txt":
+        return raw_name
+    else:
+        print("Error, the entered name is not a valid .txt format file")
+        return get_input_file()
+
+
+get_input_file()
